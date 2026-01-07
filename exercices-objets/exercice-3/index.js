97b8641bcb7e6f15a3e03575c6c80870aeaaf37d
imanehajji-tech <imane.hajji@3wa.io>
2026-01-07 14:23:48 +0000

    Sauvegarde > Exercice3 : parcourir un tableau d'objets

---
+++ exercices-objets/exercice-3/index.js
@@ -0,0 +1,22 @@
+let students = [
+	{
+		name : "Steven",
+		age : 18,
+		average : 10
+	},
+	{
+		name : "Jessica",
+		age : 20,
+		average : 13
+	},
+	{
+		name : "Charlie",
+		age : 17,
+		average : 12
+	},
+];
+
+
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i].name, students[i].age, students[i].average);
+}
